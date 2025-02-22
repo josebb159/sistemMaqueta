1341f1d052d4841769a2b307c06da51c588a6c30
jose.betancourtbruzual <jose.betancourtbruzual@gmail.com>
2025-02-21 20:03:57 -0500

    update maqueta

--- sql/usuario.sql
+++ sql/usuario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-02-2025 a las 23:09:34
+-- Tiempo de generación: 22-02-2025 a las 02:03:26
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `gec`
+-- Base de datos: `test`
 --
 
 -- --------------------------------------------------------
@@ -41,16 +41,17 @@ CREATE TABLE `usuarios` (
   `img` varchar(100) NOT NULL,
   `id_sucursal` int(11) DEFAULT NULL,
   `ip` varchar(50) DEFAULT NULL,
-  `init_sesion` datetime DEFAULT NULL
+  `init_sesion` datetime DEFAULT NULL,
+  `password_reset` varchar(15) NOT NULL,
+  `verify_password` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `id_rol`, `nombre`, `contrasena`, `email`, `fecha`, `telefono`, `fecha_registro`, `fecha_actualizacion`, `estado`, `img`, `id_sucursal`, `ip`, `init_sesion`) VALUES
-(1, 1, 'admin', '$2y$10$dom.I6UjqgGNkrhl44c1a.w.xSkObgPh5zolg8TO2vDethyzAeRT6', 'admin@gmail.com', '2024-05-01', '123', '2024-05-31 05:02:02', '2024-09-24 13:46:24', 1, 'tL3hsyvHWF8WyCRagCPR.jpg', NULL, '::1', '2025-02-03 18:33:22');
-
+INSERT INTO `usuarios` (`id`, `id_rol`, `nombre`, `contrasena`, `email`, `fecha`, `telefono`, `fecha_registro`, `fecha_actualizacion`, `estado`, `img`, `id_sucursal`, `ip`, `init_sesion`, `password_reset`, `verify_password`) VALUES
+(1, 1, 'admin', '$2y$10$dom.I6UjqgGNkrhl44c1a.w.xSkObgPh5zolg8TO2vDethyzAeRT6', 'admin@gmail.com', '2024-05-01', '123', '2024-05-31 05:02:02', '2024-09-24 13:46:24', 1, 'tL3hsyvHWF8WyCRagCPR.jpg', NULL, '::1', '2025-02-21 19:47:21', '', 0);
 
 --
 -- Índices para tablas volcadas
@@ -71,7 +72,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
